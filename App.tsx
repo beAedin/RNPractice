05c34b5993f1bc7a87687ee576682a03e659e7ed
beAedin <panda01com@g.yju.ac.kr>
2022-04-03 17:38:51 +0900

    Platform, Dimensions

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {
+  Platform,
+  Dimensions,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
 import {Colors} from 'react-native-paper';
+
+const {width, height} = Dimensions.get('window');
 
 export default function App() {
   return (
-    <SafeAreaView style={[styles.safeAreaView, {backgroundColor: '#dddddd'}]}>
-      <Text style={[styles.text, {color: 'white'}]}>Hi</Text>
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <Text style={[styles.text]}>os: {Platform.OS}</Text>
+      <Text style={[styles.text]}>width: {width} px</Text>
+      <Text style={[styles.text]}>height: {height} px</Text>
+
+      <View style={[styles.box, styles.border]}></View>
+      <View style={[styles.box, styles.border, {borderRadius: 20}]}></View>
+      <View
+        style={[
+          styles.box,
+          styles.border,
+          {borderTopLeftRadius: 40, borderBottomLeftRadius: 40},
+        ]}></View>
     </SafeAreaView>
   );
 }
-
+// prettier-ignore
 const styles = StyleSheet.create({
-  safeAreaView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: Colors.red100,
-  },
-  text: {fontSize: 20, color: Colors.blue200},
+  safeAreaView: {backgroundColor: Colors.blue500, 
+  // height: height,
+  // height: '100%',
+  flex: 1,
+  // padding: 10
+  paddingLeft: Platform.select({ios: 0, android: 20})
+},
+  text: {marginLeft: 10, fontSize: 20,  marginTop: 20},
+  box: {width: '70%', height: 100, backgroundColor: 'white', marginBottom: 10,
+  marginLeft: Platform.select({ios: 20, android: 0})},
+  border: {borderWidth:10, borderColor: Colors.lime500},
 });
